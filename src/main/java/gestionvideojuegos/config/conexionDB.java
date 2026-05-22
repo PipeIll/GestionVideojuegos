013f576bbb6fa4a56bc8b00dd2166cbcdae0559f
FELIPE <aparocio07felipe@gmail.com>
2026-05-21 22:47:13 -0500

    feat: configure database connection using system variables

---
+++ src/main/java/gestionvideojuegos/config/conexionDB.java
@@ -0,0 +1,16 @@
+package gestionvideojuegos.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexionDB {
+
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASS = System.getenv("DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
