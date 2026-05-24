e4bfd0ef197ba83f4703c2cbed02b7f6e33b64ce
FELIPE <aparocio07felipe@gmail.com>
2026-05-24 18:28:54 -0500

    feat: add database connection using .env variables
    - Successfully establish database connection.
    - Move sensitive credentials to `.env` file to prevent exposing personal data.

--- src/main/java/gestionvideojuegos/config/ConexionDB.java
+++ src/main/java/gestionvideojuegos/config/ConexionDB.java
@@ -1,16 +1,22 @@
 package gestionvideojuegos.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class conexionDB {
+public class ConexionDB {
 
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASS = System.getenv("DB_PASSWORD");
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASS = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
+        if (URL == null) {
+            throw new SQLException("No se pudo cargar las credenciales correctas de DB desde el archivo .env");
+        }
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
