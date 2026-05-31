0871599b91b6d5de367dca8d9f8eccd0ba564d5c
FELIPE <aparocio07felipe@gmail.com>
2026-05-30 19:21:14 -0500

    feat: integrate GenreDAO to display database records in UI

--- src/main/java/gestionvideojuegos/ui/VentanaGUI.java
+++ src/main/java/gestionvideojuegos/ui/VentanaGUI.java
@@ -1,9 +1,16 @@
     package gestionvideojuegos.ui;
 
+
+    import gestionvideojuegos.dao.GeneroDAO;
+    import gestionvideojuegos.model.Genero;
+    import java.util.List;
+
     import javax.swing.*;
     import java.awt.*;
 
     public class VentanaGUI extends JFrame {
+
+        private GeneroDAO generoDAO = new GeneroDAO();
 
         public VentanaGUI(){
             setTitle("Gestion de Videojuegos");
@@ -26,10 +33,22 @@
         private JPanel PanelGeneros(){
             JPanel panel = new JPanel(new BorderLayout());
 
+            List<Genero> lista = generoDAO.buscarTodos();
+
             String[] columnas = {"ID", "Nombre", "Descripción"};
-            Object[][] datos = {};
+            Object[][] datos = new Object[lista.size()][3];
+            for (int i = 0; i < lista.size(); i++ ){
+                datos[i][0] = lista.get(i).getIdGenero();
+                datos[i][1] = lista.get(i).getNombre();
+                datos[i][2] = lista.get(i).getDescripcion();
+            }
             JTable tabla = new JTable(datos, columnas);
             JScrollPane scroll =new JScrollPane(tabla);
+
+            tabla.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+            tabla.getColumnModel().getColumn(0).setPreferredWidth(50);
+            tabla.getColumnModel().getColumn(1).setPreferredWidth(150);
+            tabla.getColumnModel().getColumn(2).setPreferredWidth(750);
 
             panel.add(scroll, BorderLayout.CENTER);
             return panel;
